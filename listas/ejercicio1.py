b8a4232aab5f729c5fc225d26aa601ac236b9831
salome giraldo <marias.giraldog@upb.edu.co>
2025-09-30 11:11:44 -0500

    ejercicios listas

--- listas/ejercicio1.py
+++ listas/ejercicio1.py
@@ -1,5 +1,26 @@
+import random
 # Lista vacía
 componentes = []
+#creamos un elemento por agregar a la lista
+elemento = input("ingrese el componente del avion: ")
+#agregamos el elemento a la lista con .append()
+componentes.append(elemento)
+print(componentes)
+
+#lista de numeros
+numeros = []
+aleatorio = random.randint(0,100)
+
+#agregamos el elemento del numero aleatorio a la lista
+numeros.append(aleatorio)
+#agregamos el numero 10 a la lista
+numeros.append(10)
+#agregamos 10 numeros aleatorios a la lista
+for i in range(10):
+    aleatorio = random.randint(0,100)
+    numeros.append(aleatorio)
+
+print(numeros)
 
 # Lista con elementos
 componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
